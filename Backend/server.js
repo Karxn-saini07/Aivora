42d13ca113e8960e1e334b40f5e7a4d5ed48cc1f
Karan <karxnsaini07@gmail.com>
2026-01-25 22:24:59 +0530

    change the code

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,13 @@
-import expess from "express";
+import express from "express";
 import "dotenv/config";
 import cors from "cors";
 
-const app = expess();
+const app = express();
 const PORT = 8080;
 
+app.use(express.json());
+app.use(cors());
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
